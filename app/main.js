582f95d08d58181046e99a5a932997296e86c7b3
KarasYe <kye883883@gmail.com>
2017-06-09 11:52:10 +0800

    Add files via upload

---
+++ app/main.js
@@ -0,0 +1,17 @@
+//import './styles/appleBasket.scss'
+import React from 'react';
+import {
+	render
+} from 'react-dom';
+import {
+	Provider
+} from 'react-redux';
+import App from './containers/App.jsx';
+import configureStore from './stores/configureStore';
+const store = configureStore();
+render(
+	<Provider store={store}>
+    	<App />
+  	</Provider>,
+	document.getElementById('root')
+);
