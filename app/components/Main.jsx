eab4f1c6d44c13e875d6c6768489d3782199f563
KarasYe <kye883883@gmail.com>
2017-06-09 12:13:52 +0800

    Create Main.jsx

---
+++ app/components/Main.jsx
@@ -0,0 +1,16 @@
+//ES6
+import React from 'react';
+import OfferAndEvent from './OfferAndEvent.jsx';
+
+class Component1 extends React.Component {
+    render() {
+        return (
+        	<div>
+        		<OfferAndEvent />
+        	</div>           
+        )
+    }
+}
+
+//export Component
+export default Component1;
